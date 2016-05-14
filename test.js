76915b53952241da5b2e4c24c52bb7b892cb920f
CodeDotJS <rushforlinux@gmail.com>
2016-05-15 01:35:46 +0530

    init

---
+++ test.js
@@ -0,0 +1,12 @@
+import childProcess from 'child_process';
+import test from 'ava';
+
+test.cb(t => {
+	childProcess.execFile('./cli.js', ['-u packstat'], {
+		cwd: __dirname
+	}, (err, stdout) => {
+		t.ifError(err);
+		t.true(stdout.trim().length > 0);
+		t.end();
+	});
+});
