48dd471c5be887446c3dbd045e0295d03ca5379d
CodeDotJS <rushforlinux@gmail.com>
2016-08-04 05:46:04 +0530

    clean

--- cli.js
+++ cli.js
@@ -2,18 +2,61 @@
 
 'use strict';
 
-const whatiz = require('whatiz');
+const dns = require('dns');
+const os = require('os');
+const fs = require('fs');
+const got = require('got');
+const cheerio = require('cheerio');
+const logUpdate = require('log-update');
+const chalk = require('chalk');
+const updateNotifier = require('update-notifier');
+const pkg = require('./package.json');
 
-const colors = require('colors/safe');
+updateNotifier({pkg}).notify();
 
-const args = process.argv.slice(2);
+const arg = process.argv[2];
 
-if (process.argv.slice(2).length === 0) {
-	console.log(colors.cyan.bold('\n Usage : whatiz [package-name]\n'));
-	console.log(colors.green.bold(' ❱ Example : whatiz express\n'));
+const uniDir = '/node_modules/';
+
+const curDir = process.cwd() + uniDir + arg;
+const homeDir = os.homedir() + uniDir + arg;
+
+const lodLocal = `${curDir}${'/package.json'}`;
+const lodGlobal = `${homeDir}${'/package.json'}`;
+
+const url = `${'https://www.npmjs.com/package/'}${arg}`;
+
+const pre = chalk.bold.cyan('›');
+const fov = chalk.bold.red('›');
+
+if (!arg || arg === '-h' || arg === '--help') {
+	console.log(`
+ ${chalk.cyan('Usage   :')} ${chalk.blue('whatiz')} [${chalk.dim(' package-name ')}]
+
+ ${chalk.cyan('Example :')} ${chalk.blue('whatiz')} ${chalk.yellow('browserify')}
+	`);
 	process.exit(1);
 }
 
-const npmPack = args.toString();
-
-whatiz(npmPack).then(console.log);
+if (!fs.existsSync(curDir) && !fs.existsSync(homeDir)) {
+	dns.lookup('npmjs.com', err => {
+		if (err && err.code === 'ENOTFOUND') {
+			logUpdate(`\n${fov} Please check your internet connection\n`);
+		} else {
+			logUpdate(`\n${pre} ${arg} ${chalk.dim('is not installed, fetching description from npmjs')}\n`);
+		}
+	});
+	got(url).then(res => {
+		const $ = cheerio.load(res.body);
+		const pkgDescription = $('.package-description').text();
+		logUpdate(`\n${pre} ${pkgDescription}\n`);
+	}).catch(err => {
+		if (err) {
+			logUpdate(`\n${fov} ${chalk.dim('Package')} ${arg} ${chalk.dim('does not exist')}\n`);
+		}
+	});
+} else if (fs.existsSync(curDir)) {
+	logUpdate(`\n${pre} ${require(lodLocal).description}\n`);
+} else {
+	logUpdate(`\n${pre} ${require(lodGlobal).description}\n`);
+}
--- test.js
+++ test.js
@@ -2,11 +2,11 @@ import childProcess from 'child_process';
 import test from 'ava';
 
 test.cb(t => {
-	childProcess.execFile('./cli.js', ['whatiz'], {
+	childProcess.execFile('./cli.js', ['redux'], {
 		cwd: __dirname
 	}, (err, stdout) => {
 		t.ifError(err);
-		t.true(stdout.trim().length > 0);
+		t.true(stdout.trim() === '\u001b[?25l\n› redux is not installed, fetching description from npmjs\n\n\u001b[?25l\u001b[1000D\u001b[K\u001b[1A\u001b[1000D\u001b[K\u001b[1A\u001b[1000D\u001b[K\u001b[1A\u001b[1000D\u001b[K\n› Predictable state container for JavaScript apps\n\n\u001b[?25h');
 		t.end();
 	});
 });
